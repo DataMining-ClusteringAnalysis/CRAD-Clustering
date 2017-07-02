f9410afaa557066793e1b8c7e328258d39b35c56
DataMining-ClusteringAnalysis <xxinhuang61@gmail.com>
2017-07-02 14:46:39 -0400

    Update CRAD.py

--- CRAD.py
+++ CRAD.py
@@ -132,21 +132,4 @@ adj - An adjancey matrix which is calculated in above step.
 Output:
 An array with either a cluster id number.
 
-
-
 '''
-
-
-
-
-# data is a m by n matrix where m corresponds to number of observations, and n corresponds to number of features
-# calculate adjacency matrix where input xxDist is the distance matrix of the data using robust mahalanobis distance,
-# input StepSize as 1, and input Nbin as 200
-StepSize = 1
-Nbin = 200
-
-adj = cal_adjM_cutOff(xxDist, StepSize, Nbin)    
-y_pred = clustering_(data, adj)
-
-
-
