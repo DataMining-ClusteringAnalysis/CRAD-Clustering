da7207c479bd889566f602f7f31a31186724cd1b
DataMining-ClusteringAnalysis <xxinhuang61@gmail.com>
2017-07-02 14:39:14 -0400

    Update ExtensionToDBSCAN.py

--- ExtensionToDBSCAN.py
+++ ExtensionToDBSCAN.py
@@ -5,12 +5,9 @@ import numpy as np
 import math
 import pandas as pd
 
-Extension to DBSCAN by using CARD clustering algorithm framework
-
-
+#Extension to DBSCAN by using CARD clustering algorithm framework
 UNCLASSIFIED = False
 NOISE = 0
-
 
 def _region_query_cutOff(xxDist, point_id, MAX_STEP, n_bin):
     
@@ -48,29 +45,30 @@ def _expand_cluster(xxDist, classifications, point_id, cluster_id, MAX_STEP, n_b
             seeds = seeds[1:]
         return True
         
-def dbscan_newM(xxDist, MAX_STEP, n_bin, min_points):
-    """Implementation of Density Based Spatial Clustering of Applications with Noise
-    See https://en.wikipedia.org/wiki/DBSCAN
+def dbscan_newM(xxDist, StepSize, Nbin, min_points):
     
-    scikit-learn probably has a better implementation
-    
-    Uses Euclidean Distance as the measure
-    
-    Inputs:
-    m - A matrix whose columns are feature vectors
-    eps - Maximum distance two points can be to be regionally related
-    min_points - The minimum number of points to make a cluster
-    
-    Outputs:
-    An array with either a cluster id number or dbscan.NOISE (None) for each
-    column vector in m.
-    """
     cluster_id = 1
     n_points = xxDist.shape[0]
     classifications = [UNCLASSIFIED] * n_points
     for point_id in range(0, n_points):
         #point = m[:,point_id]
         if classifications[point_id] == UNCLASSIFIED:
-            if _expand_cluster(xxDist, classifications, point_id, cluster_id, MAX_STEP, n_bin, min_points):
+            if _expand_cluster(xxDist, classifications, point_id, cluster_id, StepSize, Nbin, min_points):
                 cluster_id = cluster_id + 1
     return classifications
+
+
+
+'''
+To perform externsion of DBSCAN, simply call the function dbscan_newM(xxDist, StepSize, Nbin, min_points)
+
+Inputs:
+xxDist - A distance matrix using robust mahalanobis distance 
+StepSize - Maximum steps of neighborhood you check in histogram to find optimal cut-off parameter
+Nbin - Number of bin in histogram to find optimal cut-off parameter
+min_points - The minimum number of points to make a cluster
+
+Outputs:
+An array with either a cluster id number or dbscan.NOISE (None) for each
+column vector in m.
+'''
